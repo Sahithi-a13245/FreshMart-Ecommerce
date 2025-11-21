cbcb0936b397197ddb267894e501fa651cb6394c
Sahithi-a13245 <mutnurusahithi@gmail.com>
2025-11-21 16:48:33 +0530

    Updated server configuration

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  credentials: true,
+}));
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("FreshMart API running");
